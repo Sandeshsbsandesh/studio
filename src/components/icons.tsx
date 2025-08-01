ee6edbc7e4dbcfaefc1ad793b969ed548d056283
Sandesh sb <sandeshsb25260@gmail.com>
2025-08-01 18:33:25 +0000

    still not looks ambulance where are wheels?

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,4 @@
+
 import type { SVGProps } from 'react';
 
 export const Icons = {
@@ -11,28 +12,30 @@ export const Icons = {
     ></svg>
   ),
   ambulance: (props: SVGProps<SVGSVGElement>) => (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="40" 
-      height="40" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M10 10H6" />
       <path d="M8 8v4" />
-      <path d="M9 18h6" />
-      <path d="M18 18h-2" />
-      <path d="M18 15h.01" />
-      <path d="M12 15h.01" />
-      <path d="M15 15h.01" />
-      <path d="M7 15h.01" />
-      <path d="M4 15h.01" />
-      <path d="M10.29 1.71 3.71 8.29a1 1 0 0 0-.3.71V17a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2h-3.34a1 1 0 0 1-1-1l-2.66-4a1 1 0 0 0-.71-.29z" />
+      <path d="M12 18h-5" />
+      <path d="M19 18h-2" />
+      <path d="M19 15h.01" />
+      <path d="m21 15-3.4-4" />
+      <path d="M15 11h-3" />
+      <path d="M3 11h7" />
+      <path d="M18 11h-2.8" />
+      <path d="M12.5 1.7 10 5H3a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h1" />
+      <circle cx="7" cy="18" r="2" />
+      <circle cx="17" cy="18" r="2" />
     </svg>
   ),
   truck: (props: SVGProps<SVGSVGElement>) => (
@@ -48,10 +51,11 @@ export const Icons = {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M5 18H3c-.6 0-1-.4-1-1V7c0-.6.4-1 1-1h10c.6 0 1 .4 1 1v11" />
-      <path d="M14 9h4l4 4v4h-8v-4c0-1.1.9-2 2-2z" />
-      <circle cx="7.5" cy="18.5" r="2.5" />
-      <circle cx="17.5" cy="18.5" r="2.5" />
+      <path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2" />
+      <path d="M15 18H9" />
+      <path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H15" />
+      <circle cx="6" cy="18" r="2" />
+      <circle cx="17" cy="18" r="2" />
     </svg>
   ),
 };

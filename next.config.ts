a913c83268da632b2a1fa30fdf333f1497d9645b
Sandesh sb <sandeshsb25260@gmail.com>
2025-10-01 07:24:15 +0000

    still same man ,check all 3 points

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -24,7 +25,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
